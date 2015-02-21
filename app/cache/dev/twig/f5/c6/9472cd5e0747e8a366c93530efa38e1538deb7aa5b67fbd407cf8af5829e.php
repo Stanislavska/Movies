fdf0c639d53e6d4a31a89ea1e3fa19f5643cb6ff
Philipp <s2793@v-ie.uek.krakow.pl>
2015-02-21 16:19:23 +0100

    first commit

---
+++ app/cache/dev/twig/f5/c6/9472cd5e0747e8a366c93530efa38e1538deb7aa5b67fbd407cf8af5829e.php
@@ -0,0 +1,31 @@
+<?php
+
+/* ArturNataliaMoviesBundle:Default:index.html.twig */
+class __TwigTemplate_f5c69472cd5e0747e8a366c93530efa38e1538deb7aa5b67fbd407cf8af5829e extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "hello natalia";
+    }
+
+    public function getTemplateName()
+    {
+        return "ArturNataliaMoviesBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/ArturNatalia/MoviesBundle/ArturNataliaMoviesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ArturNatalia\MoviesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ArturNataliaMoviesBundle extends Bundle
+{
+}
